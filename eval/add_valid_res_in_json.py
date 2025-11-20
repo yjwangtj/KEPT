d3594bb5c19d97bd2ccf5594d4740022340c8619
Yujin Wang <153347241+yjwangtj@users.noreply.github.com>
2025-11-20 22:08:12 +0800

    Update add_valid_res_in_json.py

--- eval/add_valid_res_in_json.py
+++ eval/add_valid_res_in_json.py
@@ -2,9 +2,9 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--basedata", required=True)    # 1-****_val.json
-parser.add_argument("--pred", required=True)        # 4-**output**.json
-parser.add_argument("--output", required=True)      # 5-**eval**.json
+parser.add_argument("--basedata", required=True)
+parser.add_argument("--pred", required=True)
+parser.add_argument("--output", required=True)
 args = parser.parse_args()
 
 with open(args.basedata, 'r') as f1:
@@ -21,3 +21,4 @@ for obj1, obj2 in zip(ls1, ls2):
 
 with open(args.output, "w", encoding="utf-8") as f:
     json.dump(datas, f, indent=4, ensure_ascii=False)
+
